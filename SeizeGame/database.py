9654602b8bb7443a9bb7a707bbb1fd9de335949b
Anjlnobita <jiteshkumar7200@gmail.com>
2024-12-04 21:10:11 +0530

    Update

---
+++ SeizeGame/database.py
@@ -0,0 +1,16 @@
+from motor import motor_asyncio
+from pymongo import MongoClient
+
+import config
+
+
+
+#client 1 collection db url
+Client_1 = motor_asyncio.asyncIOMotorClient(config.MONGO_DB_URI)
+db_1 = db["seize_collection"]
+
+#client 2 coin update db url
+Client_2 = motor_asynio.asyncIOMotorClient(config.MONGO_DB_UPDATE_URI)
+db_2 = db['Charector_catcher']
+
+user_collection = db["user_collection_lmaoooo"]
