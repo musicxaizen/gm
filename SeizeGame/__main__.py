5c5fc88b42b7436ae600e1d1f4ed4b772079fcad
Anjlnobita <jiteshkumar7200@gmail.com>
2024-12-07 01:37:09 +0530

    Update __main__.py

--- SeizeGame/__main__.py
+++ SeizeGame/__main__.py
@@ -1,1 +1,41 @@
- 
+import logging
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram import Update, ForceReply
+import importlib
+from config import BOT_TOKEN
+
+#logging 
+# Logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+
+#seize modules
+seize_game_modules = importlib.import_module("SeizeGame.modules")
+nguess_module = getattr(seize_game_modules, 'nguess')
+
+
+
+def main():
+    updater = Updater(BOT_TOKEN, use_context=True)
+    
+    
+    dp = updater.dispatcher
+    
+    
+    dp.add_handler(CommandHandler("nguess", nguess))
+    
+    dp.add_handler(CommandHandler("upload", upload))
+    
+    
+    
+    updater.start_polling()
+    updater.idle()
+    
+    
+if __name__ == '__main__':
+    main()
+    
+    
+
