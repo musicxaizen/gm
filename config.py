8a39dad50a4f855e8f14317ecfbe3c0771194b44
Soulaizen77 <hritikraj123412312@gmail.com>
2024-12-22 02:01:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,25 +1,33 @@
-import re
-from os import getenv
+import os
+from dotenv import load_dotenv
 
-#-------------------------------------------------------
-API_ID = int(getenv(""))
-API_HASH = getenv("")
+# Load environment variables from the .env file
+load_dotenv()
 
-#---------------------------------------------------------
-BOT_TOKEN = getenv("")
-# --------------------------------------------------------
-OWNER_ID = int(getenv("6806897901"))
-#--------------------------
-SUDOERS = int(getenv("5743956401", "6368715469", "6806897901", "6777860063"))
-#--------------------------------------------------------
-CHAT_ID = int(getenv("-1002374835450", "-1002274287324"))
-#-------------------------
-UPLOAD_CHAT_ID = int(getenv("-1002339161004"))
-#--------------------------------
-LOGGER_ID = int(getenv("-1002342285813"))
-#--------------------------------------------------------
-MONGO_DB_URI = getenv("")
-#---------------------------------------------------------
-#update coin mongodb url
-MONGO_DB_UPDATATE_URI = getenv("mongodb+srv://shivaiaxz:YfVBSatihAQlPAXc@cluster0.rqcpm.mongodb.net/?retryWrites=true&w=majority")
-#---------------------------------------------------------
+# API credentials
+API_ID = int(os.getenv("API_ID", ))  # Default value provided as 
+API_HASH = os.getenv("API_HASH", "")
+
+# Bot token
+BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+
+# Owner and Sudoers
+OWNER_ID = int(os.getenv("OWNER_ID", 6806897901))  # Default OWNER_ID
+SUDOERS = list(
+    map(int, os.getenv("SUDOERS", "5743956401,6368715469,6806897901,6777860063").split(","))
+)  # Convert to a list of integers
+
+# Chat-related IDs
+CHAT_ID = list(map(int, os.getenv("CHAT_ID", "-1002374835450,-1002274287324").split(",")))
+UPLOAD_CHAT_ID = int(os.getenv("UPLOAD_CHAT_ID", -1002339161004))
+LOGGER_ID = int(os.getenv("LOGGER_ID", -1002342285813))
+
+# MongoDB URIs
+MONGO_DB_URI = os.getenv(
+    "MONGO_DB_URI",
+    "mongodb+srv://shivaiaxz:YfVBSatihAQlPAXc@cluster0.rqcpm.mongodb.net/?retryWrites=true&w=majority",
+)
+MONGO_DB_UPDATE_URI = os.getenv(
+    "MONGO_DB_UPDATE_URI",
+    "mongodb+srv://shivaiaxz:YfVBSatihAQlPAXc@cluster0.rqcpm.mongodb.net/?retryWrites=true&w=majority",
+)
